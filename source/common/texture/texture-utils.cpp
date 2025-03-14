1cc9ec1634abadea7cc37ceae50c813f45c8526a
Mo2Hefny <moamensaied1@gmail.com>
2025-03-14 06:51:13 +0200

    feat: add sky renderer and postprocessing

--- source/common/texture/texture-utils.cpp
+++ source/common/texture/texture-utils.cpp
@@ -4,11 +4,15 @@
 #include <stb/stb_image.h>
 
 #include <iostream>
+#include <glm/glm.hpp>
 
 our::Texture2D *our::texture_utils::empty(GLenum format, glm::ivec2 size)
 {
     our::Texture2D *texture = new our::Texture2D();
     // TODO: (Req 11) Finish this function to create an empty texture with the given size and format
+    GLuint levelsCnt = glm::floor(glm::log2(glm::max<float>(size.x, size.y))) + 1;
+    glTexStorage2D(GL_TEXTURE_2D, levelsCnt, format, size.x, size.y);
+    texture->bind();
 
     return texture;
 }
@@ -28,43 +32,20 @@ our::Texture2D *our::texture_utils::loadImage(const std::string &filename, bool 
     //- 3: RGB
     //- 4: RGB and Alpha (RGBA)
     // Note: channels (the 4th argument) always returns the original number of channels in the file
-    unsigned char *pixels = stbi_load(filename.c_str(), &size.x, &size.y, &channels, 0);
+    unsigned char *pixels = stbi_load(filename.c_str(), &size.x, &size.y, &channels, 4);
     if (pixels == nullptr)
     {
         std::cerr << "Failed to load image: " << filename << std::endl;
         return nullptr;
     }
     // Create a texture
-    our::Texture2D *texture = new our::Texture2D();
-    // Bind the texture such that we upload the image data to its storage
-    // TODO: (Req 5) Finish this function to fill the texture with the data found in "pixels"
-    // Set the source image format based on the number of channels in the image
-    GLenum sourceImageFormat;
-    switch (channels)
-    {
-    case 1:
-        sourceImageFormat = GL_RED;
-        break;
-    case 2:
-        sourceImageFormat = GL_RG;
-        break;
-    case 3:
-        sourceImageFormat = GL_RGB;
-        break;
-    case 4:
-        sourceImageFormat = GL_RGBA;
-        break;
-    default:
-        sourceImageFormat = GL_RGB;
-        break;
-    }
-    // Bind the texture to GL_TEXTURE_2D
+    our::Texture2D* texture = new our::Texture2D();
+    //Bind the texture such that we upload the image data to its storage
+    //TODO: (Req 5) Finish this function to fill the texture with the data found in "pixels"
     texture->bind();
-    // Upload the image data to the texture
-    glTexImage2D(GL_TEXTURE_2D, 0, sourceImageFormat, size.x, size.y, 0, sourceImageFormat, GL_UNSIGNED_BYTE, pixels);
-    // Apply mipmapping if requested
-    if (generate_mipmap)
-    {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+
+    if (generate_mipmap) {
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     stbi_image_free(pixels); // Free image data after uploading to GPU
