c02ca3bde8a59ed238d90b02e7de3f0ff149f214
marwan2232004 <118024824+marwan2232004@users.noreply.github.com>
2025-04-12 07:15:08 +0200

    fix: wrong uniform name + missing texture activation

--- source/common/material/material.cpp
+++ source/common/material/material.cpp
@@ -3,20 +3,25 @@
 #include "../asset-loader.hpp"
 #include "deserialize-utils.hpp"
 
-namespace our {
+namespace our
+{
 
     // This function should setup the pipeline state and set the shader to be used
-    void Material::setup() const {
-        //TODO: (Req 7) Write this function
+    void Material::setup() const
+    {
+        // TODO: (Req 7) Write this function
         pipelineState.setup();
         shader->use();
     }
 
     // This function read the material data from a json object
-    void Material::deserialize(const nlohmann::json& data){
-        if(!data.is_object()) return;
+    void Material::deserialize(const nlohmann::json &data)
+    {
+        if (!data.is_object())
+            return;
 
-        if(data.contains("pipelineState")){
+        if (data.contains("pipelineState"))
+        {
             pipelineState.deserialize(data["pipelineState"]);
         }
         shader = AssetLoader<ShaderProgram>::get(data["shader"].get<std::string>());
@@ -24,42 +29,50 @@ namespace our {
     }
 
     // This function should call the setup of its parent and
-    // set the "tint" uniform to the value in the member variable tint 
-    void TintedMaterial::setup() const {
-        //TODO: (Req 7) Write this function
+    // set the "tint" uniform to the value in the member variable tint
+    void TintedMaterial::setup() const
+    {
+        // TODO: (Req 7) Write this function
         Material::setup();
         shader->set("tint", tint);
     }
 
     // This function read the material data from a json object
-    void TintedMaterial::deserialize(const nlohmann::json& data){
+    void TintedMaterial::deserialize(const nlohmann::json &data)
+    {
         Material::deserialize(data);
-        if(!data.is_object()) return;
+        if (!data.is_object())
+            return;
         tint = data.value("tint", glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
     }
 
     // This function should call the setup of its parent and
     // set the "alphaThreshold" uniform to the value in the member variable alphaThreshold
-    // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex" 
-    void TexturedMaterial::setup() const {
-        //TODO: (Req 7) Write this function
+    // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex"
+    void TexturedMaterial::setup() const
+    {
+        // TODO: (Req 7) Write this function
         TintedMaterial::setup();
         shader->set("alphaThreshold", alphaThreshold);
+        glActiveTexture(GL_TEXTURE0);
         texture->bind();
-        sampler->bind(0);
-        shader->set("texture", texture->getOpenGLName());
+        sampler->bind(GL_TEXTURE0);
+        shader->set("tex", 0);
     }
 
     // This function read the material data from a json object
-    void TexturedMaterial::deserialize(const nlohmann::json& data){
+    void TexturedMaterial::deserialize(const nlohmann::json &data)
+    {
         TintedMaterial::deserialize(data);
-        if(!data.is_object()) return;
+        if (!data.is_object())
+            return;
         alphaThreshold = data.value("alphaThreshold", 0.0f);
         texture = AssetLoader<Texture2D>::get(data.value("texture", ""));
         sampler = AssetLoader<Sampler>::get(data.value("sampler", ""));
     }
 
-    void LitMaterial::setup() const {
+    void LitMaterial::setup() const
+    {
         TexturedMaterial::setup();
         shader->set("material.useTextureAlbedo", useTextureAlbedo);
         shader->set("material.useTextureMetallicRoughness", useTextureMetallicRoughness);
@@ -71,36 +84,43 @@ namespace our {
         shader->set("material.roughness", roughness);
         shader->set("material.ambientOcclusion", ambientOcclusion);
         shader->set("material.emission", emission);
-        if(useTextureAlbedo) {
+        if (useTextureAlbedo)
+        {
             glActiveTexture(GL_TEXTURE0 + TEXTURE_UNIT_ALBEDO);
-            textureAlbedo->bind();   
+            textureAlbedo->bind();
             shader->set("material.textureAlbedo", TEXTURE_UNIT_ALBEDO);
         }
-        if(useTextureMetallicRoughness) {
+        if (useTextureMetallicRoughness)
+        {
             glActiveTexture(GL_TEXTURE0 + TEXTURE_UNIT_METALLIC_ROUGHNESS);
             textureMetallicRoughness->bind();
             shader->set("material.textureMetallicRoughness", TEXTURE_UNIT_METALLIC_ROUGHNESS);
         }
-        if(useTextureNormal) {
+        if (useTextureNormal)
+        {
             glActiveTexture(GL_TEXTURE0 + TEXTURE_UNIT_NORMAL);
-            textureNormal->bind();    
+            textureNormal->bind();
             shader->set("material.textureNormal", TEXTURE_UNIT_NORMAL);
         }
-        if(useTextureAmbientOcclusion) {
+        if (useTextureAmbientOcclusion)
+        {
             glActiveTexture(GL_TEXTURE0 + TEXTURE_UNIT_AMBIENT_OCCLUSION);
             textureAmbientOcclusion->bind();
             shader->set("material.textureAmbientOcclusion", TEXTURE_UNIT_AMBIENT_OCCLUSION);
         }
-        if(useTextureEmissive) {
+        if (useTextureEmissive)
+        {
             glActiveTexture(GL_TEXTURE0 + TEXTURE_UNIT_EMISSIVE);
             textureEmissive->bind();
             shader->set("material.textureEmissive", TEXTURE_UNIT_EMISSIVE);
         }
     }
 
-    void LitMaterial::deserialize(const nlohmann::json& data)  {
+    void LitMaterial::deserialize(const nlohmann::json &data)
+    {
         TexturedMaterial::deserialize(data);
-        if(!data.is_object()) return;
+        if (!data.is_object())
+            return;
         useTextureAlbedo = data.value("useTextureAlbedo", false);
         useTextureMetallicRoughness = data.value("useTextureMetallicRoughness", false);
         useTextureNormal = data.value("useTextureNormal", false);
@@ -114,7 +134,7 @@ namespace our {
         textureAlbedo = AssetLoader<Texture2D>::get(data.value("textureAlbedo", ""));
         textureMetallicRoughness = AssetLoader<Texture2D>::get(data.value("textureMetallicRoughness", ""));
         textureNormal = AssetLoader<Texture2D>::get(data.value("textureNormal", ""));
-        textureAmbientOcclusion = AssetLoader<Texture2D>::get(data.value("textureAmbientOcclusion", ""));   
+        textureAmbientOcclusion = AssetLoader<Texture2D>::get(data.value("textureAmbientOcclusion", ""));
         textureEmissive = AssetLoader<Texture2D>::get(data.value("textureEmissive", ""));
     }
 
